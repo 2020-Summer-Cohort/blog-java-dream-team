d5c6a71da40e3f2bd038fc02534a7ae4be898b0b
Jason Orabella <jasonorabella@gmail.com>
2020-06-21 18:52:55 -0400

    post

---
+++ src/main/java/org/wcci/blog/PostRepository.java
@@ -0,0 +1,16 @@
+package org.wcci.blog;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostRepository {
+    private PostRepository postRepo;
+
+    public PostRepository(PostRepository postRepo){
+        this.postRepo =postRepo;
+    }
+    public Post findPostbyAuthor(String author){
+        return postRepo.findPostbyAuthor(author);
+
+    }
+}
