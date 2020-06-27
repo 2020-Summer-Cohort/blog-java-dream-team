9a0426e3382cb6b44099661e53acac493e0e1520
Jason Orabella <jasonorabella@gmail.com>
2020-06-26 23:50:27 -0400

    m

--- src/main/java/org/wcci/blog/PostController.java
+++ src/main/java/org/wcci/blog/PostController.java
@@ -5,19 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class PostController {
     private PostStorage postStorage;
-//    private HashtagStorage hashtagstorage;
+    private HashtagStorage hashtagstorage;
 
-public PostController(){
-
+public PostController(PostStorage postStorage, HashtagStorage hashtagStorage){
+    this.postStorage = postStorage;
+    this.hashtagstorage = hashtagStorage;
 }
-//    private ReviewStorage reviewStorage;
-//    private HashtagsStorage hashtagsStorage;
-//
-//
-//    public ReviewsController(ReviewStorage reviewStorage, HashtagsStorage hashtagsStorage) {
-//        this.reviewStorage = reviewStorage;
-//        this.hashtagsStorage = hashtagsStorage;
-//    }
 
     public PostController(PostStorage postStorage) {
         this.postStorage = postStorage;
@@ -27,11 +20,5 @@ public PostController(){
         model.addAttribute("posts", postStorage.findPostsByTitle(postTitle));
         return "post-template";
 
-//
-//            @RequestMapping("reviews/{showTitle}")
-//            public String showSingleReview(@PathVariable String showTitle, Model model) {
-//            model.addAttribute("reviews", reviewStorage.findReviewsByShowTitle(showTitle));
-////                model.addAttribute("adultcategories", categoryStorage.findCategoriesByName(categoryName).getShows());
-//        return "review-template";
 }
 }
